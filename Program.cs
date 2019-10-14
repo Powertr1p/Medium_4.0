fbaf53c6d3fb66e9071148a9d4370510ae4156db
Roman Saykin <sc2aru@gmail.com>
2019-10-14 14:29:09 +0300

    decorator pattern

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+
+namespace Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secureLog = new A(new SecureLog(new ConsoleLog()));
+            var consoleLog = new A(new ConsoleLog());
+            var secureFileLog = new A(new SecureLog(new FileLog()));
+            var consoleAndFileLog = new A(new ConsoleLog(new FileLog()));
+            var mostSecuredLogEver = new A(new SecureLog(new ConsoleLog(new FileLog())));
+        }
+    }
+
+    interface ILogger
+    {
+        void WriteMessage(string message);
+    }
+
+    class A
+    {
+        private ILogger _log;
+
+        public A(ILogger log)
+        {
+            _log = log;
+        }
+    }
+
+    class ConsoleLog : ILogger
+    {
+        private ILogger _logger;
+        public ConsoleLog() { }
+        public ConsoleLog(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+            _logger?.WriteMessage(message);
+        }
+    }
+
+    class FileLog : ILogger
+    {
+        private ILogger _logger;
+
+        public FileLog() { }
+        public FileLog(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteMessage(string message)
+        {
+            File.WriteAllText("log.txt", message);
+            _logger?.WriteMessage(message);
+        }
+    }
+
+    class SecureLog : ILogger
+    {
+        private ILogger _logger;
+
+        public SecureLog(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteMessage(string message)
+        {
+            if (DateTime.Now.Day % 2 == 0)
+            {
+                _logger.WriteMessage(message);
+            }
+        }
+    }
+}
